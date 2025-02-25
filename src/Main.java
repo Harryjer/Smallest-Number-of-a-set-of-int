7ffc3441409b2a137b9538e0beebbeb6238023f8
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2025-02-25 23:01:22 +0530

    Take the smallest value of a given set of integers

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         /*int givenArray = {2,6,3,8,5};*/
+        Find_smallest_number small = new Find_smallest_number();
+        small.findsmallest();
     }
 
         public class FindSmallest {
